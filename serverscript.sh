aa2b6aefa46b3417292912ad9c541bfe1331f9dd
Michael Cho <77477933+michael-j-cho@users.noreply.github.com>
2021-07-26 20:32:13 -0700

    Automating server setup w/ script

---
+++ serverscript.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt install python3 python3-pip -y
+pip3 install --upgrade pip
+pip3 install flwr
+
+echo "import flwr as fl
+fl.server.start_server(config={\"num_rounds\": 3})" >> server.py
+
+python3 server.py
